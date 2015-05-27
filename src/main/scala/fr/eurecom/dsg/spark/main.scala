2fc1566ccc00d5c612ab0c48fbdc0448b992583a
Daniele Venzano <venza@brownhat.org>
2015-05-27 08:30:32 +0000

    Add more arguments and stop the spark context

--- src/main/scala/fr/eurecom/dsg/spark/main.scala
+++ src/main/scala/fr/eurecom/dsg/spark/main.scala
@@ -21,14 +21,26 @@ object BenchSQLDS {
 	  new TPCDS (
 	    sqlContext = sqlContext,
 	    sparkVersion = "1.3.1",
-	    dataLocation = args(1),
+	    dataLocation = args(0),
 	    dsdgenDir = "/usr/local/bin/",
 	    tables = tables.tables,
-	    scaleFactor = args(2))
+	    scaleFactor = args(1))
 
 	tpcds.setup()
 
-	val queries = ImpalaKitQueries
+	var queries = SimpleQueries.q7Derived
+	if (args(4) == "impalakit") {
+		queries = ImpalaKitQueries.impalaKitQueries
+	} else if (args(4) == "interactive") {
+		queries = ImpalaKitQueries.interactiveQueries
+	} else if (args(4) == "reporting") {
+		queries = ImpalaKitQueries.interactiveQueries
+	} else if (args(4) == "deepAnalytics") {
+		queries = ImpalaKitQueries.interactiveQueries
+	} else if (args(4) == "simple") {
+		queries = SimpleQueries.q7Derived
+	}
+
 	// ImpalaKitQueries contains:
 	// - interactiveQueries
 	// - reportingQueries
@@ -38,12 +50,11 @@ object BenchSQLDS {
 	// val queries = SimpleQueries
 	// SimpleQueries contains:
 	// - q7Derived (five variations of the q7 query)
-
 	val exp = tpcds.runExperiment(
-	  queries = queries.interactiveQueries,
-	  resultsLocation = args(3),
-	  includeBreakdown = true
-//	  iterations = 1,
+	  queries = queries,
+	  resultsLocation = args(2),
+	  includeBreakdown = true,
+	  iterations = args(3).toInt
 //	  variations = null,
 //	  tags = <tags of this experiment>
         )
@@ -52,5 +63,7 @@ object BenchSQLDS {
 		Thread.sleep(2000)
 	} while(exp.status == "Running")
 	println(exp)
+
+	sparkContext.stop()
     }
   }
